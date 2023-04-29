659bd09393a1b7c206649f93d38309a9a47b55db
ricardosantanadev <ricardosantanadev@gmail.com>
2023-04-29 11:33:17 -0300

    update

--- src/main/java/br/com/rsds/crudspringlistcourses/model/CoursesList.java
+++ src/main/java/br/com/rsds/crudspringlistcourses/model/CoursesList.java
@@ -1,5 +1,7 @@
 package br.com.rsds.crudspringlistcourses.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.persistence.Column;
@@ -7,15 +9,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Entity
-@Table
+// @Table(name="Cursos")
 @Data
+// @SQLDelete(sql = "") ececuta o camando sql passado na string toda vez que o metodo Delete do Repostoriry for chamado ex: deleteById(id) 
+@SQLDelete(sql = "UPDATE COURSES_LIST SET status='Inativo' WHERE id=?")
+// filtra somente os cursos com status ativo na hora do get
+@Where(clause = "status = 'Ativo'")
 public class CoursesList {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -36,4 +41,11 @@ public class CoursesList {
 	@Pattern(regexp = "Back-end|Front-end")
 	@Column(length = 10, nullable = false)
 	private String category;
+
+	@NotBlank
+	@NotNull
+	@Length(max = 10)
+	@Pattern(regexp = "Ativo|Inativo")
+	@Column(length = 10, nullable = false)
+	private String status = "Ativo";
 }
