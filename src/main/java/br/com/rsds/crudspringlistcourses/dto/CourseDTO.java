8083c50a3e78a031973df125f7dc5a52d6e03c00
ricardosantanadev <ricardosantanadev@gmail.com>
2023-11-27 17:51:12 -0300

    Validando Enums com Java Bean Validations

--- src/main/java/br/com/rsds/crudspringlistcourses/dto/CourseDTO.java
+++ src/main/java/br/com/rsds/crudspringlistcourses/dto/CourseDTO.java
@@ -4,16 +4,15 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
 
+import br.com.rsds.crudspringlistcourses.enums.Category;
+import br.com.rsds.crudspringlistcourses.enums.validation.ValueOfEnum;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
-public record CourseDTO(
-		Long id, 
-		@NotBlank @NotNull @Length(min = 5, max = 100) String name,
-		@NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") String category,
+public record CourseDTO(Long id, @NotBlank @NotNull @Length(min = 5, max = 100) String name,
+		@NotNull @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
 		/*
 		 * para exibir somente o array no console e necessario que o atributo da
 		 * interface do front-end tenha o mesmo nome do atribudo do banck-end
